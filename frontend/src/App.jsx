2b3733dc6e8501ccf4759c2fafc59e4b0126c098
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 <Waseem-Khan-Dawar>
2025-10-15 20:40:58 +0500

    Initial project commit - setup frontend folder, .gitignore, README

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+import MyAppointment from './pages/MyAppointments'
+import Contact from './pages/Contact'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/appointment' element={<Appointment />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointment />} />
+        <Route path='/appointment/:docid' element={<Appointment />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
