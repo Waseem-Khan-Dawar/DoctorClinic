8f70416eb469cc4e39d13d72461fa78bf9ba13c1
Waseem-Khan-Dawar <waseem.wm857@gmail.com>
2025-10-21 16:23:08 +0500

    feat(NavBar): added onClick navigation to 'Create account' button and integrated useNavigate for login redirection

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <div className='flex item-center justify-between text-sm py-4 mb-4 border-b border-b-gray-400'>
       <img className='w-44 cursor-pointer' src={assets.logo} alt=" " />
@@ -19,12 +20,14 @@ const NavBar = () => {
           <li className='py-1'>ABOUT</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
-        <NavLink to={'/contact'} end>
-          <li className='py-1'>CONTACT</li>
+        <NavLink to={'/contact'}>
+          <li className='py-1'>CONTACT end</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
       </ul>
-      <button>Create account</button>
+      <div className='flex items-center gap-4'>
+        <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+      </div>
     </div>
   )
 }
